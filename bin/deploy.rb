4c1ebdc4acb0029829c27223176d61b31fcd4141
corcorans <corcorans13@gmail.com>
2013-11-02 12:47:41 -0500

    Fixed spacing and syntax.

--- bin/deploy.rb
+++ bin/deploy.rb
@@ -1,5 +1,6 @@
 def rmdir_r(dir)
   Dir.chdir(dir)
+  
   Dir['**/{*,.*}'].select do |f|
     n = f.split('/').last
     n != '..' and n != '.'
@@ -10,25 +11,30 @@ def rmdir_r(dir)
       File.delete(f)
     end
   end
+  
   Dir.delete(dir)
 end
 
 def main
   dotfiles = File.join(Dir.home, 'dotfiles')
   entries = Dir.entries(dotfiles).select { |f| f[0] != '.' }
+  
   existing = entries.select do |f|
     p = File.join(Dir.home, '.' + f)
     File.exists?(p) and !File.symlink?(p)
   end
-  response = 'y'
+  
   if !existing.empty?
     puts 'WARNING: This will overwrite the following dotfiles:'
+    
     existing.each do |f|
       puts (File.directory?(f) ? 'd ' : 'f ') + f
     end
+    
     print 'Are you sure you want to overwrite these files? (y/n) '
-    response = gets
+    response ||= gets
   end
+  
   if !entries.empty? and response[0].downcase == 'y'
     entries.reject!{ |f| ['bin', 'README.md'].include?(f) || f[0] == '.' }.each do |f|
       p = File.join(Dir.home, '.' + f)
